d3768d1bad4969ee84e9f8d5a58d206e31724d0a
Diogo Santos <dioggopsanttos@gmail.com>
2021-09-08 00:03:19 +0100

    Styles in borders

--- src/components/Cell.styled.js
+++ src/components/Cell.styled.js
@@ -4,7 +4,7 @@ export const CellStyled = styled.div`
   display: flex;
   flex: 1;
   margin: 1px;
-
+  border: 1px solid hsl(0 0% 70%);
   background-color: ${({ isAlive }) =>
     isAlive === true ? `hsl(0 0% 70%)` : `hsl(0 0% 0%)`};
 `;
--- src/components/Grid.styled.js
+++ src/components/Grid.styled.js
@@ -5,6 +5,7 @@ export const GridContainer = styled.div`
   flex-direction: column;
   width: ${({ gridStyleSize }) => gridStyleSize}vh;
   height: ${({ gridStyleSize }) => gridStyleSize}vh;
+  outline: 1px solid hsl(0 0% 70%);
 `;
 
 export const GridRow = styled.div`
